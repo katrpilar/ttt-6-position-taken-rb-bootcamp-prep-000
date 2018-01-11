f5bfc3da5d1d6c67a9cbbd6fe389014295234633
Katrina Brinson <kbrinso@gmail.com>
2018-01-11 18:51:02 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken(board,index)
   if board.length == 0
-    
+    return
   elsif (board[index] == " ") || (board[index] == "")
     return false
   else
